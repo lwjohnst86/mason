04fcd86404e6469aec734988b47410b12fe06f3d
Luke W Johnston <lwjohnst@gmail.com>
2016-05-17 00:07:31 -0400

    Added roxygen docs to add_variables

--- R/add_variables.R
+++ R/add_variables.R
@@ -1,5 +1,24 @@
 
+#' Add variables to the analysis
+#'
+#' While different analyses use different types of variables, in general they
+#' can be classified as in the 'y' or 'x' position of a statistical equation.
+#' They can further be classified as covariates and as an interaction term.
+#'
+#' @param blueprint The blueprint object
+#' @param type The variable type, i.e. where it is located on the equation (y
+#'   position, x, as a covariate, etc.)
+#' @param variables Variables to use for the type specified
+#'
+#' @return Adds variables to the blueprint
 #' @export
+#'
+#' @examples
+#'
+#' ds <- design(iris, 'cor')
+#' ds <- add_variables(ds, 'xvar', 'Sepal.Length')
+#' ds <- add_variables(ds, 'yvar', 'Petal.Length')
+#'
 add_variables <- function(blueprint,
                           type = c('yvars', 'xvars', 'covariates', 'interaction'),
                           variables) {
