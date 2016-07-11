71f95078c79d434783c905729faee2d4fa6144cb
Luke W Johnston <lwjohnst@gmail.com>
2016-07-10 20:55:29 -0400

    Updated test for t tests

--- tests/testthat/test-ttest.R
+++ tests/testthat/test-ttest.R
@@ -5,13 +5,17 @@ context("t-test output")
 ds <- design(testdata, 't.test')
 ds <- add_settings(ds)
 ds <- add_variables(ds, 'yvars', 'Income')
-test_that("(t.test) variables are in data", {
-    ds_wrong <- add_variables(ds, 'xvars', c('Population', 'doesntexist'))
+test_that("(for t.test) construct needs yvar or xvar", {
+    ds_wrong <- add_variables(ds, 'xvars', c('Population', 'Murder'))
+    expect_error(contruct(ds_wrong))
+
+    ds_wrong <- add_variables(ds, 'yvars', c('Population', 'Murder'))
     expect_error(construct(ds_wrong))
 })
 
-test_that("(t.test) variables are the same type", {
-    ds_wrong <- add_variables(ds, 'xvars', c('Income', 'Rich'))
+test_that("(for t.test) construct variables the same in yvar and xvar", {
+    ds_wrong <- add_variables(ds, 'yvars', c('Income', 'Area'))
+    ds_wrong <- add_variables(ds_wrong, 'xvars', c('Income', 'Population'))
     expect_error(construct(ds_wrong))
 })
 
@@ -24,8 +28,7 @@ ds <- add_variables(ds, 'yvars', vars[1:2])
 ds <- add_variables(ds, 'xvars', vars[3:4])
 
 test_that("(t.test) results compare to real results", {
-    test_results <- construct(ds)$results
-    test_results <- dplyr::rename(test_results, estimate1 = ymean, estimate2 = xmean)
+    test_results <- scrub(construct(ds))[-1:-2]
     real_results <- rbind(
         broom::tidy(t.test(testdata$Income, testdata$Area)),
         broom::tidy(t.test(testdata$Income, testdata$Frost)),
@@ -33,12 +36,13 @@ test_that("(t.test) results compare to real results", {
         broom::tidy(t.test(testdata$Population, testdata$Frost))
     ) %>%
         dplyr::tbl_df()
-    expect_equal(test_results[c(5, 2, 4, 6:10)], real_results)
+    expect_equal(test_results, real_results)
 })
 
 # scrub and polish --------------------------------------------------------
 
 ds <- construct(ds)
-test_that("(for cor) scrub converts to tbl_df", {
+test_that("(for t.test) scrub converts to tbl_df and removes specs", {
     expect_is(scrub(ds), 'tbl_df')
+    expect_null(attr(scrub(ds), 'specs'))
 })
