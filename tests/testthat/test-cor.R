cd2bf5654aa0c0d323b6c20056ff9774e3ab7641
Luke W Johnston <lwjohnst@gmail.com>
2016-07-10 20:29:07 -0400

    Updated test for cor analysis

--- tests/testthat/test-cor.R
+++ tests/testthat/test-cor.R
@@ -1,26 +1,15 @@
 context("Correlation output")
 
-# construct_table ---------------------------------------------------------
+# construct ---------------------------------------------------------------
 
 ds <- design(testdata, 'cor')
 ds <- add_settings(ds)
-test_that("(cor) needs at least xvar, maybe yvar", {
+test_that("(cor) needs at least xvar", {
     ds_wrong <- add_variables(ds, 'yvars', 'Income')
     expect_error(construct(ds_wrong))
 })
 
-test_that("(cor) variables are in data", {
-    ds_wrong <- add_variables(ds, 'xvars', c('Income', 'Population', 'doesntexist'))
-    expect_error(construct(ds_wrong))
-})
-
-test_that("(cor) variables are the same type", {
-    ds_wrong <- add_variables(ds, 'xvars', c('Income', 'Rich'))
-    expect_error(construct(ds_wrong))
-})
-
 # comparing to real results -----------------------------------------------
-
 
 ds <- design(testdata, 'cor')
 ds <- add_settings(ds)
@@ -29,7 +18,7 @@ ds <- add_variables(ds, 'xvars', vars)
 
 test_that("(cor) results compare to real results", {
     cor_function <- function(x, y = NULL) {
-        cor_data <- cor(x, y,
+        cor_data <- stats::cor(x, y,
                         use = 'complete.obs',
                         method = 'pearson')
         if (is.null(y))
@@ -37,13 +26,13 @@ test_that("(cor) results compare to real results", {
         dplyr::tbl_df(as.data.frame(cor_data))
     }
 
-    test_results <- construct(ds)$results
+    test_results <- attr(construct(ds), 'specs')$results
     real_results <- cor_function(testdata[vars])
     expect_equal(test_results[-1], real_results)
 
     yvars <- c('Murder', 'HS.Grad', 'Illiteracy')
     ds <- add_variables(ds, 'yvars', yvars)
-    test_results <- construct(ds)$results
+    test_results <- attr(construct(ds), 'specs')$results
     real_results <- cor_function(testdata[vars], testdata[yvars])
     expect_equal(test_results[-1], real_results)
 })
@@ -51,6 +40,7 @@ test_that("(cor) results compare to real results", {
 # scrub and polish --------------------------------------------------------
 
 ds <- construct(ds)
-test_that("(for cor) scrub converts to tbl_df", {
+test_that("(for cor) scrub converts to tbl_df and removes specs", {
     expect_is(scrub(ds), 'tbl_df')
+    expect_null(attr(scrub(ds), 'specs'))
 })
