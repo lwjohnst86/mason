77575389e08755903a92d0b7209ab24c1b57f513
Luke W Johnston <lwjohnst@gmail.com>
2016-07-10 20:22:27 -0400

    Complete revamp of add_variables to reflect new make_blueprint

--- R/add_variables.R
+++ R/add_variables.R
@@ -15,24 +15,45 @@
 #'
 #' @examples
 #'
-#' ds <- design(iris, 'cor')
-#' ds <- add_variables(ds, 'xvar', 'Sepal.Length')
-#' ds <- add_variables(ds, 'yvar', 'Petal.Length')
+#' library(magrittr)
+#' ds <- design(iris, 'cor') %>%
+#'  add_settings()
+#' add_variables(ds, 'xvar', 'Sepal.Length')
+#' add_variables(ds, 'yvar', 'Petal.Length')
 #'
 #' ds <- design(iris, 't.test')
 #' ds <- add_variables(ds, 'yvar', c('Sepal.Length', 'Sepal.Width'))
 #' ds <- add_variables(ds, 'xvar', 'Petal.Length')
 #'
-add_variables <- function(blueprint,
+add_variables <- function(data,
                           type = c('yvars', 'xvars', 'covariates', 'interaction'),
                           variables) {
-    .is_blueprint(blueprint)
-    type <- match.arg(type)
-
-    if (exists(type, blueprint))
-        message(paste0(type, ' has already been added to blueprint... replacing it'))
-
-    blueprint[[type]] <- variables
-    return(blueprint)
+    UseMethod('add_variables', data)
 }
+
+#' @export
+add_variables.default <-
+    function(data,
+             type = c('yvars', 'xvars', 'covariates', 'interaction'),
+             variables) {
+
+        type <- match.arg(type)
+        if (missing(variables))
+            stop('Please supply one or more variables.')
+        vars_exist(data, variables)
+
+        if (any(c('xvars', 'yvars') %in% type)) {
+            var.type <- unique(sapply(data[variables], class))
+            if (length(var.type) > 1)
+                stop('Please do not mix numeric and character/factor variables',
+                     ' in the ', type, '.', call. = FALSE)
+        }
+
+        var.specs <- attr(data, 'specs')$vars
+        if (!is.null(var.specs))
+            if (type %in% names(var.specs))
+                message(type, 'already exists in the specs, but will be replaced.')
+
+        make_blueprint(data, vars = setNames(list(variables), type))
+    }
 
