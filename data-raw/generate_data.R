5a6a4347d707c331a964f32c49de946a0e052447
Luke W Johnston <lwjohnst@gmail.com>
2017-07-28 10:19:20 -0400

    in internal dataset, don't have factors

--- data-raw/generate_data.R
+++ data-raw/generate_data.R
@@ -1,7 +1,8 @@
 library(dplyr)
 testdata <- data.frame(state.region, state.x77) %>%
-    dplyr::mutate(Rich = ifelse(Income > 4600, 'yes', 'no')) %>%
-    dplyr::tbl_df() %>%
-    dplyr::arrange(state.region)
+    mutate(Rich = as.numeric(Income > 4500)) %>%
+    tbl_df() %>%
+    arrange(state.region) %>%
+    mutate(Populated = as.numeric(Population > 3000))
 
 devtools::use_data(testdata, overwrite = TRUE, internal = TRUE)
