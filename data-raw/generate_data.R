bdcdd250a706aa001d091d46781c88070e9afced
Luke W. Johnston <lwjohnst@gmail.com>
2020-06-03 18:22:52 +0200

    moved testdata over into helper function

--- data-raw/generate_data.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-testdata <- data.frame(state.region, state.x77) %>%
-    mutate(Rich = as.numeric(Income > 4500)) %>%
-    tibble::as_tibble() %>%
-    arrange(state.region) %>%
-    mutate(Populated = as.numeric(Population > 3000))
-
-devtools::use_data(testdata, overwrite = TRUE, internal = TRUE)
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,4 +1,10 @@
 
+testdata <- data.frame(state.region, state.x77) %>%
+    dplyr::mutate(Rich = as.numeric(Income > 4500)) %>%
+    tibble::as_tibble() %>%
+    dplyr::arrange(state.region) %>%
+    dplyr::mutate(Populated = as.numeric(Population > 3000))
+
 fix_order <- function(.tbl) {
     .tbl %>%
         dplyr::arrange(term, estimate)
